6a5f21514f539e69a8ef3b869ed1c7c9afdd885f
Genisenko <geniscattkd@gmail.com>
2023-06-13 23:31:08 +0200

    First commit, hello from Home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello from Home</h1>
+      <p>Why doesn't work?</p>
+    </div>
+  );
+};
+
+export default Home;
