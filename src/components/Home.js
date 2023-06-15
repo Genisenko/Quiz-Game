482201a991f77c3eea4802f1cbda72d0d7360a72
Genisenko <geniscattkd@gmail.com>
2023-06-15 11:53:54 +0200

    Home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Helmet } from 'react-helmet';
+import {BiCube} from 'react-icons/bi';
+ import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Hello from Home</h1>
-      <p>Why doesn't work?</p>
+const Home = () => (
+  <Fragment>
+  {/* Helmet tag changes the title of the tab */}
+    <Helmet><title>Quiz App - Home</title></Helmet> 
+    <div id="home" >
+      <section>
+        <div>
+            <BiCube size=' 48px'/>
+        </div>
+        <h1>Quiz App</h1>
+        <div className='play-buttun-container'>
+          <ul>
+            <li><Link to="/play/instructions">Play</Link></li>
+          </ul>
+        </div>
+        <div className='auth-container'>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+          
+        </div>
+
+      </section>
     </div>
+  </Fragment>
+
   );
-};
 
 export default Home;
