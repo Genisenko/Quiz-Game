74ac74cc85ec8d4fad7481c408597dc5d978038d
Genisenko <geniscattkd@gmail.com>
2023-06-15 17:14:30 +0200

    Home styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React, {Fragment} from 'react';
 import { Helmet } from 'react-helmet';
 import {BiCube} from 'react-icons/bi';
- import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => (
   <Fragment>
@@ -9,18 +10,18 @@ const Home = () => (
     <Helmet><title>Quiz App - Home</title></Helmet> 
     <div id="home" >
       <section>
-        <div>
-            <BiCube size=' 48px'/>
+        <div style={{textAlign: 'center'}}>
+            <BiCube className='cube' size='8rem' color='orange'/>
         </div>
         <h1>Quiz App</h1>
-        <div className='play-buttun-container'>
+        <div className='play-button-container'>
           <ul>
-            <li><Link to="/play/instructions">Play</Link></li>
+            <li><Link className='play-button' to="/play/instructions">Play</Link></li>
           </ul>
         </div>
         <div className='auth-container'>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        <Link to="/login" className='auth-buttons' id='login-button'>Login</Link>
+        <Link to="/register" className='auth-buttons' id='signup-button' >Register</Link>
           
         </div>
 
